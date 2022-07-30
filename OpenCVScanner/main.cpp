38931a0de08a78370f02501d59c51e6fe6803d67
liya60188 <lina.yanouri@gmail.com>
2022-07-30 17:25:35 +0200

    final modifications

--- OpenCVScanner/main.cpp
+++ OpenCVScanner/main.cpp
@@ -7,36 +7,33 @@ using namespace std;
 using namespace cv;
 
 String imageName;
-Mat src, imgGrayscale, imgCannyEdge, imgGaussblur, dilatedImg, imgTreshold, matrix, imgwarp, img_final;
-vector<Point> initialpoints, docpoints;
-float w = 420, h = 596;
+Mat src, grayscaleImg, cannyEdgeImg, gaussBlurImg, matrix, imgwarp, finalImg;
+vector<Point> initialpoints, doc_points;
+
 
 ///// Identifying the document to scan /////
-Mat identify(Mat image){
-    //converting to greyscale
-    cvtColor(image, imgGrayscale, COLOR_BGR2GRAY);
-    //blurring
-    GaussianBlur(imgGrayscale, imgGaussblur, Size(3, 3), 3, 3);
-    //edge detection
-    Canny(imgGaussblur, imgCannyEdge, 122, 231);
-    Mat kernel = getStructuringElement(MORPH_RECT, Size(2, 2));
-    dilate(imgCannyEdge, dilatedImg, kernel);
-    return dilatedImg;
+Mat edgesDetection(Mat image){
+    //Grayscale
+    cvtColor(image, grayscaleImg, COLOR_BGR2GRAY);
+    //Blurring
+    GaussianBlur(grayscaleImg, gaussBlurImg, Size(3, 3), 3, 3);
+    //Edge detection
+    Canny(gaussBlurImg, cannyEdgeImg, 122, 231);
 
+    return cannyEdgeImg;
 }
 
 ///// Get contour of the document /////
 vector<Point> getcontours(Mat image){
-    vector<vector<Point>> contours;
+    vector<vector<Point>> contours, contour;
     vector<Vec4i> hierarchy;
     findContours(image, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-
+    
     vector<vector<Point>> conpoly(contours.size());
-
-    vector<Point> bigger;
+    vector<Point> drawing;
     int maxArea = 0;
 
-    //Not detect the noise in the image
+    //To not detect the noise in the image
     for (int i = 0; i < contours.size(); i++)		
     {
         int area = contourArea(contours[i]);
@@ -48,23 +45,25 @@ vector<Point> getcontours(Mat image){
 
             if (area > maxArea && conpoly[i].size() == 4)
             {
-                bigger = { conpoly[i][0] , conpoly[i][1], conpoly[i][2], conpoly[i][3] };
+                drawing = { conpoly[i][0] , conpoly[i][1], conpoly[i][2], conpoly[i][3] };
                 maxArea = area;
             }
         }
     }
-    return bigger;
+
+    return drawing;
 }
 
-void drawpoints(vector<Point> points, Scalar Color)
+///// Drawing the points on the image /////
+void drawpoints(Mat image, vector<Point> points, Scalar Color)
 {
     for (int i = 0; i < points.size(); i++)
     {
         circle(src, points[i], 5, Color, FILLED);
-        putText(src, to_string(i), points[i], FONT_HERSHEY_DUPLEX, 1, Color, 3);
     }
 }
 
+///// Reorder the points /////
 vector<Point> reorder(vector<Point> points)
 {
     vector<Point> newpoints;
@@ -83,6 +82,7 @@ vector<Point> reorder(vector<Point> points)
     return newpoints;
 }
 
+///// Warping to get the top-down view of the document /////
 Mat warping(Mat img, vector<Point> point, float w, float h)
 {
     Point2f src[4] = { point[0], point[1] ,point[2], point[3] };
@@ -94,7 +94,7 @@ Mat warping(Mat img, vector<Point> point, float w, float h)
 }
 
 int main(int, char**) {
-
+    ///// Menu /////
     do {
         cout << "Select the image you want to scan :\n"
             "- Brouillon1\n"
@@ -120,30 +120,21 @@ int main(int, char**) {
         waitKey(0);
     } while (src.empty());
     
+    ///// Resizing the image /////
     resize(src, src, Size(src.cols / 2.5, src.rows / 2.5));
-
     ///// Identify document /////
-    imgTreshold = identify(src);
-    
-
+    Mat imgTreshold = edgesDetection(src);
     ///// Get contours of document /////
     initialpoints = getcontours(imgTreshold);
-    //drawpoints(initialpoints, Scalar(0, 0, 255));
-    docpoints = reorder(initialpoints);
-    
+    ///// Reorder points /////
+    doc_points = reorder(initialpoints);
     ///// Warp /////
-    img_final = warping(src, docpoints, w, h);
-    drawpoints(docpoints, Scalar(255, 0, 255));
+    float w = 420, h = 596;
+    finalImg = warping(src, doc_points, w, h);
 
-    //// Crop the image /////
-    int cropVal = 10;
-    Rect roi(cropVal, cropVal, w - (2 * cropVal), h - (2 * cropVal));
-    Mat imgcrop = img_final(roi);
 
     imshow("Original", src);
-    //imshow("Image defined", dilatedImg);
-    //imshow("Image final", imgwarp);
-    imshow("Final", imgcrop);
+    imshow("Final Image", finalImg);
     waitKey(0);
 
     return 0;
